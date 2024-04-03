0d8d1a7a5060330e117db7454406e4991b4976ed
Ultra-pn4rv <tsarultra@gmail.com>
2024-04-03 09:58:49 -0400

    questao 9

---
+++ questao9.js
@@ -0,0 +1,10 @@
+/*
+9. Questão: Escreva um código que imprima os números de 0 a 100 em 
+incrementos de 10.
+*/
+let contador = 0;
+
+while (contador < 101) {
+    console.log(+contador);
+    contador+=10;
+}
