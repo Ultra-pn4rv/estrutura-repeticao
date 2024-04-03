48c16d284577d7516b681a9d89f2cbc983a01056
Ultra-pn4rv <tsarultra@gmail.com>
2024-04-03 09:58:57 -0400

    questao 10

---
+++ questao10.js
@@ -0,0 +1,10 @@
+/*
+10. Questão: Escreva um código que imprima a tabuada do 5 (de 1 a 10).
+*/
+let contador = 0;
+
+while (contador < 11) {
+    let resultado = 5 * contador
+    console.log("5 x " + contador + " = " + resultado);
+    contador++;
+}
