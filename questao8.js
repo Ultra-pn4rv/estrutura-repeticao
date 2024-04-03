7c79ee1fd4b2ce8bf08c2d48435272d4fd33e25c
Ultra-pn4rv <tsarultra@gmail.com>
2024-04-03 09:58:45 -0400

    questao 8

---
+++ questao8.js
@@ -0,0 +1,10 @@
+/*
+8. Questão: Escreva um código que imprima os números de 10 a 1 em 
+ordem decrescente.
+*/
+let contador = 10;
+
+while (contador > 0) {
+    console.log(+contador);
+    contador--;
+}
